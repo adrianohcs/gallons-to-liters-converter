0cbfeabe53911fca7e554d4b6408e5e00a4caac6
Adriano <adriano.henrique.eng@gmail.com>
2021-09-07 02:18:14 -0300

    Set the precision to 2 decimal places and changed the table of conversions (from 1 to 100 gallons).

--- src/unitconverter/GallonToLiter.java
+++ src/unitconverter/GallonToLiter.java
@@ -6,9 +6,9 @@ public class GallonToLiter {
         int count = 0;
 
         // 1 liter equals 3.7854 gallons
-        for (gallons = 1; gallons <= 20; gallons++) {
+        for (gallons = 1; gallons <= 100; gallons++) {
             liters = gallons * 3.7854;
-            System.out.println(gallons + " gallons equals " + liters + " liters.");
+            System.out.println(String.format("%.2f", gallons) + " gallons equals " + String.format("%.2f", liters) + " liters.");
 
             // Adding a line counter
             count++;
