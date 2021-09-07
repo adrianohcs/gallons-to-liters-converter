0a28dab3db951fa97a6cf5e847e5794728c98a2a
Adriano <adriano.henrique.eng@gmail.com>
2021-09-07 02:05:35 -0300

    The file outputs a blank line every 10th line (close to a table converter format).

--- src/unitconverter/GallonToLiter.java
+++ src/unitconverter/GallonToLiter.java
@@ -5,15 +5,20 @@ public class GallonToLiter {
         double gallons, liters;
         int count = 0;
 
-        // 1 liter equals 3.7854 gallons.
+        // 1 liter equals 3.7854 gallons
         for (gallons = 1; gallons <= 20; gallons++) {
             liters = gallons * 3.7854;
             System.out.println(gallons + " gallons equals " + liters + " liters.");
 
             // Adding a line counter
             count++;
-            System.out.println("LOOP ITERATION: " + count);
-            System.out.println();
+
+            // Every 10th line, output a blank line
+            if (count == 10) {
+                System.out.println();
+                // Reset the line counter
+                count = 0;
+            }
         }
     }
 }
