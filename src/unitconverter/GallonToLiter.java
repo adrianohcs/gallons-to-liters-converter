cda76f2c2432ecb98446b5086290ae057a6ec64c
Adriano <adriano.henrique.eng@gmail.com>
2021-09-07 02:00:30 -0300

    Successfully added a line counter.

--- src/unitconverter/GallonToLiter.java
+++ src/unitconverter/GallonToLiter.java
@@ -3,12 +3,17 @@ package unitconverter;
 public class GallonToLiter {
     public static void main(String[] args) {
         double gallons, liters;
-        int count;
+        int count = 0;
 
         // 1 liter equals 3.7854 gallons.
-        for (gallons = 0; gallons <= 20; gallons++) {
+        for (gallons = 1; gallons <= 20; gallons++) {
             liters = gallons * 3.7854;
             System.out.println(gallons + " gallons equals " + liters + " liters.");
+
+            // Adding a line counter
+            count++;
+            System.out.println("LOOP ITERATION: " + count);
+            System.out.println();
         }
     }
 }
