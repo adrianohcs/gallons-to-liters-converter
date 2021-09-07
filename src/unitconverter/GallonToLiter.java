1eda1df4ed709ef524d2565ec26438e1449ef556
Adriano <adriano.henrique.eng@gmail.com>
2021-09-07 01:53:14 -0300

    Running the file and displaying each loop iteration.

--- src/unitconverter/GallonToLiter.java
+++ src/unitconverter/GallonToLiter.java
@@ -2,5 +2,13 @@ package unitconverter;
 
 public class GallonToLiter {
     public static void main(String[] args) {
+        double gallons, liters;
+        int count;
+
+        // 1 liter equals 3.7854 gallons.
+        for (gallons = 0; gallons <= 20; gallons++) {
+            liters = gallons * 3.7854;
+            System.out.println(gallons + " gallons equals " + liters + " liters.");
+        }
     }
 }
